e54d64bba3ee38b81651078ff6f9c64740f481bc
hanafiroua <hanafiroua788@gmail.com>
2026-05-02 12:54:43 +0100

    put , delete , getByGenre film

--- src/main/java/gl2/exemple/cinema/Controller/FilmController.java
+++ src/main/java/gl2/exemple/cinema/Controller/FilmController.java
@@ -1,9 +1,9 @@
 package gl2.exemple.cinema.Controller;
 
-
 import gl2.exemple.cinema.Model.Film;
 import gl2.exemple.cinema.Repository.FilmRepository;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -25,9 +25,36 @@ public class FilmController {
     public Film add(@RequestBody Film film) {
         return filmRepository.save(film);
     }
+
     @GetMapping("/{id}")
     public Film getById(@PathVariable Long id) {
         return filmRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Film non trouvé"));
+    }
+
+    @PutMapping("/{id}")
+    public Film update(@PathVariable Long id, @RequestBody Film nouveauFilm) {
+        Film film = filmRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Film non trouvé"));
+
+        film.setTitre(nouveauFilm.getTitre());
+        film.setGenre(nouveauFilm.getGenre());
+        film.setDuree(nouveauFilm.getDuree());
+
+        return filmRepository.save(film);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        Film film = filmRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Film non trouvé"));
+
+        filmRepository.delete(film);
+        return "Film supprimé avec succès";
+    }
+
+    @GetMapping("/genre/{genre}")
+    public List<Film> getByGenre(@PathVariable String genre) {
+        return filmRepository.findByGenre(genre);
     }
 }
--- src/main/java/gl2/exemple/cinema/Repository/FilmRepository.java
+++ src/main/java/gl2/exemple/cinema/Repository/FilmRepository.java
@@ -2,6 +2,8 @@ package gl2.exemple.cinema.Repository;
 
 import gl2.exemple.cinema.Model.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface FilmRepository extends JpaRepository<Film, Long> {
+    List<Film> findByGenre(String genre);
 }
